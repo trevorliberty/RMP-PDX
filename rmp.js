7dabdfa7210f7551fdd4bfacfe5fe3025ad124aa
stackoverjoe <jml27@pdx.edu>
2019-12-19 17:34:57 -0800

    Review now showing again - popups need rework -- RMP redesigned entire website

--- rmp.js
+++ rmp.js
@@ -3,8 +3,7 @@
  *  Waits for DOM to be rendered
  *  When user clicks 'Search', waits for selector to be initialized
  */
-$(document).ready(function () {
-
+$(document).ready(function() {
   $(
     "#search-go, #s2id_txt_subject, #txt_courseNumber, #txt_keywordlike, #txt_keywordexact"
   ).on("keypress click", function test(e) {
@@ -20,7 +19,7 @@ $(document).ready(function () {
         } else {
           timerId = setTimeout(handle, 500);
         }
-      }, 500);
+      }, 1000);
     }
   });
 });
@@ -29,19 +28,19 @@ $(document).ready(function () {
  * \n character in each block  */
 function startScript() {
   const arr = $("td:nth-child(11)");
-  Array.from(arr).forEach(function (subgroup) {
+  Array.from(arr).forEach(function(subgroup) {
     var nameStr = subgroup.innerText;
     //if there is more than 1 professor:
     var nameSplit = nameStr.split("\n");
     //if nameSplit[] has newline char at lastindex
     --nameSplit.length;
     //Anonymous function with nameSplit[i]
-    Array.from(nameSplit).forEach(function (professor) {
+    Array.from(nameSplit).forEach(function(professor) {
       getTID(professor)
-        .then(function (tid) {
+        .then(function(tid) {
           return getRatingLink(tid);
         })
-        .then(function (ratingLink) {
+        .then(function(ratingLink) {
           return embedLink(subgroup, ratingLink);
         })
         .catch(e => {
@@ -64,24 +63,32 @@ function getTID(professor) {
     .trim();
   /**Returns the TID of the professor if they exist in rate my professor */
   return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage({
+    chrome.runtime.sendMessage(
+      {
         method: "POST",
         url: "http://www.ratemyprofessors.com/search.jsp",
         data: `queryBy=teacherName&query=portland+state+university+${lastName}+${firstName}&facetSearch=true`
       },
-      function (response) {
+      function(response) {
+        //console.log(response);
         if (response) {
           const regex = new RegExp(lastName + "\\W?,\\W?" + firstName, "ig");
           const doc = document.createElement("html");
-          const cleaned = response.slice(0, response.indexOf(`<div class="mobileAppPromo">`));
+          const cleaned = response.slice(
+            0,
+            response.indexOf(`<div class="mobileAppPromo">`)
+          );
+
           doc.innerHTML = cleaned;
           const anchors = doc.getElementsByTagName("a");
-          Array.from(anchors).forEach(function (anchor) {
+          Array.from(anchors).forEach(function(anchor) {
             if (anchor.innerHTML.match(regex)) {
-              const tid = anchor.getAttribute("href").match(/[0-9]{1,}/g);
+              tid = anchor.getAttribute("href").match(/[0-9]{1,}/g);
+              console.log("TTTIIDDD" + tid);
               resolve(tid ? tid[0] : null);
             }
           });
+          //return;
         }
       }
     );
@@ -94,22 +101,36 @@ function getTID(professor) {
  * @returns {object}
  */
 function getRatingLink(tid) {
+  console.log("TID" + tid);
   const url = "http://www.ratemyprofessors.com/ShowRatings.jsp";
   return new Promise((resolve, reject) => {
-    chrome.runtime.sendMessage({
-        method: "POST",
-        url: `${url}`, //"http://www.ratemyprofessors.com/ShowRatings.jsp?tid=",
-        data: `tid=${tid}`
+    chrome.runtime.sendMessage(
+      {
+        method: "GET",
+        url: `${url}?tid=${tid}` //"http://www.ratemyprofessors.com/ShowRatings.jsp?tid=",
+        //data: `tid=${tid}`
       },
-      function (response) {
+      function(response) {
+        //console.log(response);
         if (response) {
-          const cleaned = response.slice(0, response.indexOf(`<div class="mobileAppPromo">`));
-          const element = cleaned.match(
-            /<div class="grade" title="">[0-9.]{3}<\/div>/g
+          const cleaned = response.slice(
+            0,
+            response.indexOf(`<div class="mobileAppPromo">`)
           );
-          const rating = element ? element[0].match(/[0-9.]{3}/g) : 'No Reviews';
+          element = cleaned.match(
+            /div class="RatingValue__Numerator-.*">[0-9.]{3}<\/div>/g
+          )[0];
+          element = element.match(/[0-9.]{3}/g);
+          //element = $(`[class^="RatingValue"`).innerText;
+          //console.log("RATING" + element[0]);
+          if (element) {
+            console.log(element);
+          }
+          const rating = element
+            ? element[0].match(/[0-9.]{3}/g)
+            : "No Reviews";
           const link = `${url + "?tid=" + tid}`;
-          const rateLink = `https://www.ratemyprofessors.com/AddRating.jsp?tid=${tid}`
+          const rateLink = `https://www.ratemyprofessors.com/AddRating.jsp?tid=${tid}`;
 
           var block = $(
             "#mainContent > div.right-panel > div.rating-breakdown",
@@ -137,9 +158,9 @@ function getRatingLink(tid) {
 function getPopup(block) {
   var str = block.substring(block.indexOf("Overall Quality")).trim();
   var arr = str.split("\n");
-  (function () {
+  (function() {
     arr = arr.map(el => el.trim());
-    arr = arr.filter(function (e) {
+    arr = arr.filter(function(e) {
       return (
         e &&
         e != "See how other students describe this professor." &&
@@ -149,11 +170,11 @@ function getPopup(block) {
   })();
   var Tags = arr.splice(6);
   Tags = Tags.map(tag => {
-    return `<li>${tag}</li>`
-  })
-  Tags.length = 4
-  Tags.shift()
-  Tags = Tags.join(` `)
+    return `<li>${tag}</li>`;
+  });
+  Tags.length = 4;
+  Tags.shift();
+  Tags = Tags.join(` `);
   const embed = {
     overall: arr[0] + ": " + arr[1],
     takeAgain: arr[2] + ": " + arr[3],
@@ -177,9 +198,9 @@ function embedLink(professor, ratingLink) {
       ${professor.innerText}
       <a id="${ratingLink.URL}" class="popUp" href=${ratingLink.URL} target="_blank" style="color: #${hex}" visited="color: #${hex}">(${ratingLink.rate})
                 </a>`;
-      let stuff = ['tooltipster-noir'];
+      let stuff = ["tooltipster-noir"];
       const tipContent = getContent(ratingLink, stuff);
-      if (ratingLink.rate !== 'No Reviews') {
+      if (ratingLink.rate !== "No Reviews") {
         $(professor).tooltipster({
           interactive: true,
           title: "hello",
@@ -190,11 +211,10 @@ function embedLink(professor, ratingLink) {
           },
           theme: stuff,
           contentAsHTML: true,
-          content: tipContent,
+          content: tipContent
         });
       }
     } else {
-
       console.log(`Already embedded rating for ${professor.innerText}`);
     }
   }
@@ -205,17 +225,29 @@ function embedLink(professor, ratingLink) {
  * @param {array} themeArray
  */
 function getContent(ratingLink, themeArray) {
-  let shadow = '';
+  let shadow = "";
   let tempObj = ratingLink.popUp;
   if (ratingLink.rate >= 4) {
-    themeArray.push('tooltipster-noir-thing', 'tooltipster-noir-arrBody1', 'tooltipster-noir-arrBorder1');
-    shadow = "#afcb34"
+    themeArray.push(
+      "tooltipster-noir-thing",
+      "tooltipster-noir-arrBody1",
+      "tooltipster-noir-arrBorder1"
+    );
+    shadow = "#afcb34";
   } else if (ratingLink.rate >= 3) {
-    themeArray.push('tooltipster-noir-thing1', 'tooltipster-noir-arrBody2', 'tooltipster-noir-arrBorder2');
-    shadow = "#dec048"
+    themeArray.push(
+      "tooltipster-noir-thing1",
+      "tooltipster-noir-arrBody2",
+      "tooltipster-noir-arrBorder2"
+    );
+    shadow = "#dec048";
   } else {
-    themeArray.push('tooltipster-noir-thing2', 'tooltipster-noir-arrBody3', 'tooltipster-noir-arrBorder3');
-    shadow = "#c73454"
+    themeArray.push(
+      "tooltipster-noir-thing2",
+      "tooltipster-noir-arrBody3",
+      "tooltipster-noir-arrBorder3"
+    );
+    shadow = "#c73454";
   }
   var tipContent = `
     <div>
@@ -228,19 +260,19 @@ function getContent(ratingLink, themeArray) {
       <h3>
         ${tempObj.difficulty}
       </h3>
-    </div>`
+    </div>`;
   if (tempObj.tags.length > 20) {
     tipContent += `
          <h3 style="margin: 0; padding:0">Top tags:</h3>
           <ul style="padding-bottom: 20px; padding-left: 0; margin:0; text-shadow:2px 2px ${shadow}">
           ${tempObj.tags}
           </ul>
-          `
+          `;
   }
   tipContent += `
         <div><a style="color:rgb(253, 253, 253)" target="_blank" href="${ratingLink.URL}#sratingComments">Go to professor's comments</a></div>
         <div><a style="	color: #ffffff" target="_blank" href="${ratingLink.rateUrl}#rateProfessorForm">Rate this professor</a></div>
-    `
+    `;
   return tipContent;
 }
 
